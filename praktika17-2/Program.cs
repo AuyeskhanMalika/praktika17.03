c34b9cb23c9b7a3a312219a55c6b3f9081c75f91
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-10 23:03:37 +0600

    Добавьте файлы проекта.

---
+++ praktika17-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika17_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(List<>);
+            ConstructorInfo[] constructors = type.GetConstructors();
+            Console.WriteLine("Конструкторы List<>: ");
+            foreach (var construct in constructors)
+            {
+                Console.WriteLine(construct.ToString());
+            }
+            Console.Read();
+        }
+    }
+    }
+}
